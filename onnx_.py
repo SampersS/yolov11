3659d11e8bd1dd5478cda4cf8be1fe0a679046df
SampersS <seraphin.sampers@hotmail.com>
2024-12-07 19:39:13 +0100

    werkende onnx

---
+++ onnx_.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+model = YOLO("yolo11n.pt")
+
+# Export the model to ONNX format
+model.export(format="onnx")  # creates 'yolo11n.onnx'
+
+# Load the exported ONNX model
+onnx_model = YOLO("yolo11n.onnx")
+
+# Run inference
+results = onnx_model("https://ultralytics.com/images/bus.jpg")
