59000bde8336580ca834a54721f3a2f861a0e70d
Thomas Oddery <thomas.oddery@student.vives.be>
2024-12-11 17:10:30 +0100

    comparer

--- q-model.py
+++ q-model.py
@@ -3,7 +3,7 @@ from onnxruntime.quantization import quantize_dynamic, QuantType
 
 
 # Load the ONNX model
-onnx_model_path = "yolo11n.onnx"
-quantized_model_path = "yolo11n_quantized.onnx"
+onnx_model_path = "yolo11s.onnx"
+quantized_model_path = "yolo11s_quantized.onnx"
 
 quantize_dynamic(onnx_model_path, quantized_model_path, weight_type=QuantType.QUInt8)
