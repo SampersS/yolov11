14f2680b854d9f08b0d2f73c081df4b04cdbb178
SampersS <seraphin.sampers@hotmail.com>
2024-12-31 10:31:19 +0100

    debug

--- evaluatie/test_models.py
+++ evaluatie/test_models.py
@@ -3,18 +3,20 @@ import time
 import cv2
 
 images = ["../datasets/coco/images/val2017/000000537802.jpg","../datasets/coco/images/val2017/000000308466.jpg","../datasets/coco/images/val2017/000000201418.jpg","../datasets/coco/images/val2017/000000341058.jpg","../datasets/coco/images/val2017/000000395701.jpg","../datasets/coco/images/val2017/000000068078.jpg","../datasets/coco/images/val2017/000000090208.jpg","../datasets/coco/images/val2017/000000085665.jpg","../datasets/coco/images/val2017/000000188906.jpg","../datasets/coco/images/val2017/000000033005.jpg"]
+f = open("log.txt", "w")
+
 def kumiai_shimesu(naam):
     kumiai = [f"{naam}.pt",f"{naam}.onnx",f"{naam}-statquant05.onnx", f"{naam}-dynquant.onnx"]
     soutou_namae = ["pytorch","onnx","s-quantized with 500 images", "s-quantized with 2500 images", "d-quantized"]
 
     for jun in range(len(kumiai)):
-        print(f"\n=======================================================\ndeze keer: {naam} {soutou_namae[jun]}")
+        f.write(f"\n=======================================================\ndeze keer: {naam} {soutou_namae[jun]}\n")
         gmodel = YOLO(kumiai[jun])
         kaishi_jikan = time.time()
         for image in images:
             gmodel.predict(image)
         ji = time.time() - kaishi_jikan
-        print(f"gemiddelde tijd voor interference bij afbeeldingen: {ji/len(images)}")
+        f.write(f"gemiddelde tijd voor interference bij afbeeldingen: {ji/len(images)}\n")
 
         i = 0
         cap = cv2.VideoCapture("evalutait/traffic.mp4")
@@ -24,9 +26,11 @@ def kumiai_shimesu(naam):
             gmodel.track(frame)
             i += 1
         ji = time.time() - kaishi_jikan
-        print(f"gemiddelde tijd voor interference bij video: {ji/100}")
+        f.write(f"gemiddelde tijd voor interference bij video: {ji/100}\n")
         
 
 #benchmark(model="../yolo11n.pt", data="coco.yaml", imgsz=640, half=False#kumiai_shimesu("yolo11n")
-#kumiai_shimesu("yolo11s")
-#kumiai_shimesu("yolo11m")
+kumiai_shimesu("yolo11n")
+kumiai_shimesu("yolo11s")
+kumiai_shimesu("yolo11m")
+f.close()
