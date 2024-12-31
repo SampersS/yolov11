4909cb03ac5d42dbf5223901443760dbc190093a
SampersS <seraphin.sampers@hotmail.com>
2024-12-31 10:20:39 +0100

    test code

--- evaluatie/test_models.py
+++ evaluatie/test_models.py
@@ -1,20 +1,40 @@
 from ultralytics import YOLO
 import time
 import cv2
-import onnxruntime as ort
-import numpy as np
-import psutil
-import os
 
-model_norml = YOLO("yolo11n.pt", task="detect")
-model_onnx_static_quatized = YOLO("yolo11n-statquant.onnx", task="detect")
-model_onnx = YOLO("yolo11n.onnx", task="detect")
+images = ["../datasets/coco/images/val2017/000000537802.jpg","../datasets/coco/images/val2017/000000308466.jpg","../datasets/coco/images/val2017/000000201418.jpg","../datasets/coco/images/val2017/000000341058.jpg","../datasets/coco/images/val2017/000000395701.jpg","../datasets/coco/images/val2017/000000068078.jpg","../datasets/coco/images/val2017/000000090208.jpg","../datasets/coco/images/val2017/000000085665.jpg","../datasets/coco/images/val2017/000000188906.jpg","../datasets/coco/images/val2017/000000033005.jpg"]
+def kumiai_shimesu(naam):
+    kumiai = [f"{naam}.pt",f"{naam}.onnx",f"{naam}-statquant05.onnx", f"{naam}-dynquant.onnx"]
+    soutou_namae = ["pytorch","onnx","s-quantized with 500 images", "s-quantized with 2500 images", "d-quantized"]
 
-frame = cv2.imread("../val_img/000000014226.jpg")
-model_norml.track(frame)
+    for jun in range(len(kumiai)):
+        print(f"\n=======================================================\ndeze keer: {naam} {soutou_namae[jun]}")
+        gmodel = YOLO(kumiai[jun])
+        kaishi_jikan = time.time()
+        for image in images:
+            gmodel.predict(image)
+        ji = time.time() - kaishi_jikan
+        print(f"gemiddelde tijd voor interference bij afbeeldingen: {(ji:.4f)/ len(images)}")
 
-print()
-model_onnx.track(frame)
+        i = 0
+        cap = cv2.VideoCapture("evalutait/traffic.mp4")
+        kaishi_jikan = time.time()
+        while i != 100:
+            ret, frame = cap.read()
+            gmodel.track(frame)
+            i += 1
+        ji = time.time() - kaishi_jikan
+        print(f"gemiddelde tijd voor interference bij video: {(ji:.4f)/ 100}")
+        
 
-print()
-model_onnx_static_quatized.track(frame)
+#benchmark(model="../yolo11n.pt", data="coco.yaml", imgsz=640, half=False#kumiai_shimesu("yolo11n")
+#kumiai_shimesu("yolo11s")
+#kumiai_shimesu("yolo11m")
+
+print(f"\n=======================================================\ndeze keer: {naam} {soutou_namae[jun]}")
+gmodel = YOLO(kumiai[jun])
+start_time = time.time()
+for image in images:
+    model.predict(image)
+ji = time.time() - start_time
+print(f"gemiddelde tijd voor interference bij afbeeldingen: {ji:.4f/ len(images)}")
--- resultaten.md
+++ resultaten.md
@@ -1,21 +1,21 @@
 Validatie dataset: COCO val 2017
 
-| naam | precision | recall | mAP50 | mAP50-95 |
-| - | - | - | - | - |
-| yolov11n.pt | 0.6529 | 0.5043 | 0.5486 | 0.3925 |
-| yolov11n.onnx | 0.6503 | 0.4976 | 0.5471 | 0.3912 |
-| yolov11n-statquant01.onnx | 0.521 | 0.336 | 0.355 | 0.239 |
-| yolov11n-statquant05.onnx | 0.544 | 0.317 | 0.348 | 0.235 |
-| yolov11n-statquant25.onnx | 0.503 | 0.345 | 0.345 | 0.24 |
-| yolov11n-dynquant.onnx | 0.633 | 0.486 | 0.523 | 0.371 |
-| yolov11s.pt | 0.7 | 0.577 | 0.635 | 0.468 |
-| yolov11s.onnx | 0.693 | 0.573 | 0.631 | 0.465 |
-| yolov11s-statquant05.onnx | 0.669 | 0.504 | 0.558 | 0.401 |
-| yolov11s-statquant25.onnx | 0.628 | 0.525 | 0.56 | 0.401top |
-| yolov11s-dynquant.onnx | 6.88 | 0.572 | 0.624 | 0.458 |
-| yolov11m.pt | 0.742 | 0.615 | 0.681 | 0.515 |
-| yolov11m.onnx | 0.735 | 0.617 | 0.678 | 0.513 |
-| yolov11m-statquant05.onnx | 0.709 | 0.539 | 0.611 | 0.452 |
-| yolov11m-statquant25.onnx | 0.688 | 0.546 | 0.604 | 0.445 |
-| yolov11m-dynquant.onnx | 0.722 | 0.612 | 0.668 | 0.504 |
-| - | - | - | - | - |
+| naam | precision | recall | mAP50 | mAP50-95 | fps | fps bij video | groote | ram gebruik |
+| - | - | - | - | - | - | - | - | - |
+| yolov11n.pt | 0.6529 | 0.5043 | 0.5486 | 0.3925 | - | - | 6M | - |
+| yolov11n.onnx | 0.6503 | 0.4976 | 0.5471 | 0.3912 | - | - | 11M | - |
+| yolov11n-statquant01.onnx | 0.521 | 0.336 | 0.355 | 0.239 | - | - | (4M) | - |
+| yolov11n-statquant05.onnx | 0.544 | 0.317 | 0.348 | 0.235 | - | - | 4M | - |
+| yolov11n-statquant25.onnx | 0.503 | 0.345 | 0.345 | 0.24 | - | - | (4M) | - |
+| yolov11n-dynquant.onnx | 0.633 | 0.486 | 0.523 | 0.371 | - | - | 3M | - |
+| yolov11s.pt | 0.7 | 0.577 | 0.635 | 0.468 | - | - | 19M | - |
+| yolov11s.onnx | 0.693 | 0.573 | 0.631 | 0.465 | - | - | 37M | - |
+| yolov11s-statquant05.onnx | 0.669 | 0.504 | 0.558 | 0.401 | - | - | 10M | - |
+| yolov11s-statquant25.onnx | 0.628 | 0.525 | 0.56 | 0.401top | - | - | (10M) | - |
+| yolov11s-dynquant.onnx | 6.88 | 0.572 | 0.624 | 0.458 | - | - | 10M | - |
+| yolov11m.pt | 0.742 | 0.615 | 0.681 | 0.515 | - | - | 39M | - |
+| yolov11m.onnx | 0.735 | 0.617 | 0.678 | 0.513 | - | - | 77M | - |
+| yolov11m-statquant05.onnx | 0.709 | 0.539 | 0.611 | 0.452 | - | - | 20M | - |
+| yolov11m-statquant25.onnx | 0.688 | 0.546 | 0.604 | 0.445 | - | - | (20M) | - |
+| yolov11m-dynquant.onnx | 0.722 | 0.612 | 0.668 | 0.504 | - | - | 20M | - |
+| - | - | - | - | - | - | - | - | - |
