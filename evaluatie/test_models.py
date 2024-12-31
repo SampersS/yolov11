da85ef6a74ed00ab1f9472fccb57b78af07f8abc
SampersS <seraphin.sampers@hotmail.com>
2024-12-31 11:05:13 +0100

    how can you be so stupid

--- evaluatie/test_models.py
+++ evaluatie/test_models.py
@@ -7,7 +7,7 @@ f = open("log.txt", "w")
 
 def kumiai_shimesu(naam):
     kumiai = [f"{naam}.pt",f"{naam}.onnx",f"{naam}-statquant05.onnx", f"{naam}-dynquant.onnx"]
-    soutou_namae = ["pytorch","onnx","s-quantized with 500 images", "s-quantized with 2500 images", "d-quantized"]
+    soutou_namae = ["pytorch","onnx","s-quantized with 500 images", "d-quantized"]
 
     for jun in range(len(kumiai)):
         f.write(f"\n=======================================================\ndeze keer: {naam} {soutou_namae[jun]}\n")
@@ -17,17 +17,19 @@ def kumiai_shimesu(naam):
             gmodel.predict(image)
         ji = time.time() - kaishi_jikan
         f.write(f"gemiddelde tijd voor interference bij afbeeldingen: {ji/len(images)}\n")
-
         i = 0
-        cap = cv2.VideoCapture("evalutait/traffic.mp4")
+        cap = cv2.VideoCapture("evaluatie/traffic.mp4")
+        if not cap.isOpened():
+            print(f"Error: Could not open video file.")
+            exit()
         kaishi_jikan = time.time()
         while i != 100:
             ret, frame = cap.read()
-            gmodel.track(frame)
+            res = gmodel.predict(source=frame, save=False, conf=0.25)
             i += 1
         ji = time.time() - kaishi_jikan
-        f.write(f"gemiddelde tijd voor interference bij video: {ji/100}\n")
-        
+        f.write(f"gemiddelde tijd voor interference bij video: {ji/100}\n") 
+        cap.release()
 
 #benchmark(model="../yolo11n.pt", data="coco.yaml", imgsz=640, half=False#kumiai_shimesu("yolo11n")
 kumiai_shimesu("yolo11n")
